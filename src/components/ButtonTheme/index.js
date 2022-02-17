a489b66ae5b64773d2fcfe2f10f81e9001e08883
jose.ocana <jose.ocana@i3television.es>
2022-02-17 18:00:46 +0100

    Añadido Eslint y Prettier y eliminado Typescript

--- src/components/ButtonTheme/index.js
+++ src/components/ButtonTheme/index.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 import Button from '@material-ui/core/Button';
+import { theme } from '../../theme';
 
-const StyledButton = styled(Button)`
+const StyledButtonTheme = styled(Button)`
   font-size: 1em;
   margin: 1em;
   padding: 0.25em 1em;
   border-radius: 3px;
+  background-color: ${theme.palette.primary.main};
 `;
 
-export default StyledButton;
+export default StyledButtonTheme;
--- src/components/Buttton/index.js
+++ src/components/Buttton/index.js
@@ -1,13 +1,11 @@
 import styled from 'styled-components';
 import Button from '@material-ui/core/Button';
-import { theme } from '../../theme';
 
-const StyledButtonTheme = styled(Button)`
+const StyledButton = styled(Button)`
   font-size: 1em;
   margin: 1em;
   padding: 0.25em 1em;
   border-radius: 3px;
-  background-color: ${theme.palette.primary.main};
 `;
 
-export default StyledButtonTheme;
+export default StyledButton;
